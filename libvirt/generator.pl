9551e5dea512c7a009cac426569cedae6f300ce5
Pino Toscano <ptoscano@redhat.com>
2019-08-29 15:34:52 +0200

    build: stop generating a config.h
    It basically contains only PACKAGE_* defines, which are not used in the
    C parts.  Hence, do not make autoconf generate one.
    
    Signed-off-by: Pino Toscano <ptoscano@redhat.com>

--- libvirt/generator.pl
+++ libvirt/generator.pl
@@ -232,8 +232,6 @@ print F <<'END';
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
  */
 
-#include "config.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
